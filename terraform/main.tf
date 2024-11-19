6e7ddaecd2979ef68d0f72cb01ea4c98b7be1a83
KatarzynaBanach <katarzyna.banach98@gmail.com>
2024-11-19 14:16:41 +0100

    fix: terraform updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
 provider "google" {
   project = "gifted-object-406811"
   region  = "us-central1"
+  zone    = "us-central1-a"
 }
 
 resource "google_storage_bucket" "us_bucket" {
@@ -14,6 +23,41 @@ resource "google_storage_bucket" "eu_bucket" {
   location = "EU"
   storage_class = "STANDARD"
 }
+
+
+# TRY:
+
+# resource "google_storage_bucket" "bucket" {
+#   for_each  = toset( ["EU", "US"] )
+#   name      = "bucket_bq_data_${each.value}"
+
+#   location  = each.value
+#   storage_class = "STANDARD"
+# }
+
+# resource "google_bigquery_dataset" "dataset" {
+#   for_each  = toset( ["EU", "US"] )
+#   dataset_id      = "dataset${each.value}"
+#   location        = each.value
+# }
+
+# resource "google_storage_bucket" "test-bucket-for-state" {
+#   name        = "qwiklabs-gcp-00-081e872498d7"
+#   location    = "US" # Replace with EU for Europe region
+#   uniform_bucket_level_access = true
+# }
+# terraform {
+#   backend "gcs" {
+#     bucket  = "qwiklabs-gcp-00-081e872498d7"
+#     prefix  = "terraform/state"
+#   }
+# }
+
+# potem w konsoli:
+
+# terraform init -migrate-state
+
+
 
 resource "google_bigquery_dataset" "dataset_us" {
   dataset_id                  = "dataset_us"
